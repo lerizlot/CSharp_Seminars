de114342c5d7d6f807b1fff0e1f751ac5fe67087
Lera <lerizlot@icloud.com>
2022-11-26 14:57:25 +0400

    Выполнена задача #11

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,4 +1,18 @@
 ﻿// Задача №11. Напишите программу, которая выводит случайное трёхзначное число
 // и удаляет вторую цифру этого числа
 
+Random random = new Random();
+int number = random.Next(100, 1000);
+
+Console.WriteLine(number);
+
+int a = number / 100;
+int b = number % 10;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine("Необходимое число = " +a +b);
+
+
+
 
