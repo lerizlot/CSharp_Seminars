b1099a2f145de8689762d059bfc232b1a224e8ca
Lera <lerizlot@icloud.com>
2022-11-26 14:18:20 +0400

    Minor addots to Task11

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,12 +1,15 @@
-﻿Random random = new Random();
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99]
+// и показывает наибольшую цифру числа
+
+Random random = new Random();
 int number = random.Next(10, 100);
+
+Console.WriteLine("Random number = {0}", number);
+//Console.WriteLine($"Random number = {number}");
+//Console.WriteLine("Random number " + number);
 
 int a = number / 10;
 int b = number % 10;
-
-Console.WriteLine("Random number = {0}", number, a, b);
-Console.WriteLine($"Random number = {number}");
-Console.WriteLine("Random number " + number);
 
 if (a > b)
     Console.WriteLine("a > b, {0}", a);
