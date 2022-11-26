94fd1d37c4b4037e9559d2a20545353ba7d64362
Lera <lerizlot@icloud.com>
2022-11-26 13:30:24 +0400

    Добавлены задачи из семинара #1

---
+++ Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//3 -> Среда 
+//5 -> Пятница
+
+Console.WriteLine("Введите день недели");
+int DayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+switch (DayOfWeek)
+{
+    case 1:
+        Console.WriteLine("It's Monday!");
+        break;
+    case 2:
+        Console.WriteLine("It's Tuesday!");
+        break;
+    case 3:
+        Console.WriteLine("It's Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("It's Thursday");
+        break;
+    case 5:
+        Console.WriteLine("It's Friday");
+        break;
+    default:
+        Console.WriteLine("No Day");
+        break;
+}
---
+++ Task5/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача №5. Напишите программу,
+// которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке
+// от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for( int P = -N; P < N; P++)
+{
+    Console.WriteLine(P);
+};
+Console.WriteLine(N);
+
+// альтернативное решение
+
+Console.WriteLine("Введите число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 0)
+for (int n_opposite = -n; n_opposite <= n; n_opposite++)
+{
+    Console.WriteLine(n_opposite);
+}
+else 
+for (int n_opposite = n; n_opposite <= -n; n_opposite++)
+{
+    Console.WriteLine(n_opposite);
+}
