8a7cbd41b4f00ade377426362a1d66b678d37979
Lera <lerizlot@icloud.com>
2022-11-28 21:26:45 +0400

    Выполнена задача #22

---
+++ Task22/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i <= N; i++)
+    Console.WriteLine(Math.Pow(i, 2));
