739c1501c1a53996599eaf188f52f14143664759
Lera <lerizlot@icloud.com>
2022-12-04 16:44:46 +0400

    Решена задача #26 двумя вариантами

---
+++ Task26/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите ваше число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(num.ToString().Length);
+
+// альтернативное решение
+
+Console.WriteLine("Введите ваше число 2");
+string number = Console.ReadLine();
+int count = 0;
+char[] array = new char[number.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = number[i];
+    count++;
+}
+Console.WriteLine("Количество цифр " +count);
